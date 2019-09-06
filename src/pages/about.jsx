512f310ee983d184ad44db2113b9d1ede0c9cf80
Karey Higuera <karey.higuera@gmail.com>
2019-09-06 14:16:40 -0500

    add small summary to about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,9 +2,12 @@ import React from "react"
 import Layout from "../components/Layout";
 import { graphql } from "gatsby"
 
+import '../css/about.css';
+
 export default ({ data }) => (
   <Layout>
     <h1 className="page-title">About {data.site.siteMetadata.title}</h1>
+    <h4 className="about-title">This site is a personal project of mine to share what I learn in my studies. It is also a repository for different projects that I have created that I feel may be of use to others.</h4>
   </Layout>
 )
 export const query = graphql`
