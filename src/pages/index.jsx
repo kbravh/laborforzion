272455f901af4a72d6d2f2d91a1d4bbededbf284
Karey Higuera <karey.higuera@gmail.com>
2019-09-08 16:07:29 -0500

    update favicon to red

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,13 +13,13 @@ export default ({ data }) => {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#fbd303" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#d04925" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <meta name="apple-mobile-web-app-title" content="Labor For Zion" />
         <meta name="application-name" content="Labor For Zion" />
-        <meta name="msapplication-TileColor" content="#fbd303" />
+        <meta name="msapplication-TileColor" content="#d04925" />
         <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-        <meta name="theme-color" content="#fbd303" />
+        <meta name="theme-color" content="#d04925" />
       </Helmet>
       <div>
         <h1 className="page-title">{data.site.siteMetadata.title}</h1>
